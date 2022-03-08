eeac3e696b77e445b4eec992842c7dbefa18a38c
Petr Stepanov <stepanovps@gmail.com>
2022-03-08 18:35:07 -0500

    Trigger setup

--- test.py
+++ test.py
@@ -13,9 +13,9 @@ def writeFileHeader(inst):
 #def readDecode(scope, string):
 #    # https://pyvisa.readthedocs.io/en/latest/introduction/communication.html
 #    # Query '?' tests both reading and writing    
-#    scope.write(string)
+#    instr.write(string)
 #    # We need to decode string in Python3: https://stackabuse.com/convert-bytes-to-string-in-python/
-#    output = scope.read()
+#    output = instr.read()
 #    print(type(output))
 #    # str(output, 'UTF-8')
 #    return output
@@ -41,72 +41,92 @@ print(rm.list_resources())
 # Ethernet (WORKS): https://github.com/pyvisa/pyvisa-py/issues/261
 addr = 'TCPIP::192.168.100.36::INSTR'
 
-scope = rm.open_resource(addr)
+# Open oscilloscope
+with rm.open_resource(addr) as instr:
+    # Configure the instrument
+    # https://pyvisa.readthedocs.io/en/latest/introduction/communication.html#getting-the-instrument-configuration-right
+    # p.2-5 Programmers manual: 
+    # | This oscilloscope does not support ASCII LF only message termination. The
+    # | oscilloscope always terminates outgoing messages with LF and EOI.
+    instr.read_termination = '\n'   # '\n\r', '\r\n', '\r' not work 
+    instr.write_termination = '\n'
+    
+    # Obtain oscilloscope identification code (manual, p.2-350):
+    instr.write('*IDN?')        # 
+    scopeCode = instr.read()
+    print(scopeCode)
+    
+    # Tip: instr.query('*IDN?') is same as:
+    # instr.write('*IDN?')
+    # print(instr.read())
+    
+    # TEST: read raw data - success
+    instr.write('CURV?')
+    data = instr.read_raw()
+    
+    # TEST (FAIL): if oscilloscope is configured to transfer data in ASCII
+    # values = instr.query_ascii_values('CURV?')
+    # print(values)
+    
+    # TEST: if oscilloscope is configured to transfer data in ASCII
+    # Binary data typ[es list: https://docs.python.org/3/library/struct.html#format-characters
+    channels = [1, 2]
+    now = datetime.now()    # remember current time
+    
+    plt.clf()               # configure plot
+    plt.grid(True)
+    plt.xlabel('Channel')
+    plt.ylabel('Amplitude, mV')
+    fileName="/cuanas/Data/" + now.strftime("%Y-%m-%d-%H:%M:%S")
+    
+    # save CSV file
+    values = []
+    instr.write('SAVe:WAVEform:FILEFormat SPREADSheet')
+    
+    for channel in channels:
+        # Select source
+        instr.write('DATa:SOUrce CH' + str(channel))
+        values = instr.query_binary_values('CURV?', datatype='b', is_big_endian=True)
+        print(values)
+    
+        # Construct filename
+    
+        # Plot valaues
+        plt.plot(values)
+    
+    # TODO: use SAVe:WAVEform instead - it creates header automatically
+    instr.write('SAVe:WAVEform ALL, "E:/' + fileName + '.csv')
+    
+    plt.savefig(fileName + '.png')
+    # plt.show()
+    
+    # Start infinite loop
+    #i=0
+    #while i < 1:
+        #instr.assert_trigger()
+        #instr.wait_on_event
+        #instr.write('TRIGger:STATE?')
+        #triggerState = instr.read()
+        #print(triggerState)
+    
+    # Trigger Event Handling
+    # https://pyvisa.readthedocs.io/en/latest/introduction/event_handling.html
+    event_type = pyvisa.constants.EventType.sservice_request
+    # Mechanism by which we want to be notified
+    event_mech = pyvisa.constants.EventMechanism.queue
 
-# Configure the instrument
-# https://pyvisa.readthedocs.io/en/latest/introduction/communication.html#getting-the-instrument-configuration-right
-# p.2-5 Programmers manual: 
-# | This oscilloscope does not support ASCII LF only message termination. The
-# | oscilloscope always terminates outgoing messages with LF and EOI.
-scope.read_termination = '\n'   # '\n\r', '\r\n', '\r' not work 
-scope.write_termination = '\n'
+    instr.enable_event(event_type, event_mech)
 
-# Obtain oscilloscope identification code (manual, p.2-350):
-scope.write('*IDN?')        # 
-scopeCode = scope.read()
-print(scopeCode)
+    # Instrument specific code to enable service request
+    # (for example on operation complete OPC)
+    instr.write("*SRE 1")
+    instr.write("INIT")
 
-# Tip: scope.query('*IDN?') is same as:
-# scope.write('*IDN?')
-# print(instr.read())
+    # Wait for the event to occur
+    response = instr.wait_on_event(event_type, 1000)
+    assert response.event.event_type == event_type
+    assert response.timed_out == False
+    instr.disable_event(event_type, event_mech)    
 
-# TEST: read raw data - success
-scope.write('CURV?')
-data = scope.read_raw()
-
-# TEST (FAIL): if oscilloscope is configured to transfer data in ASCII
-# values = scope.query_ascii_values('CURV?')
-# print(values)
-
-# TEST: if oscilloscope is configured to transfer data in ASCII
-# Binary data typ[es list: https://docs.python.org/3/library/struct.html#format-characters
-channels = [1, 2]
-now = datetime.now()    # remember current time
-
-plt.clf()               # configure plot
-plt.grid(True)
-plt.xlabel('Channel')
-plt.ylabel('Amplitude, mV')
-fileName="/cuanas/Data/" + now.strftime("%Y-%m-%d-%H:%M:%S")
-
-# save CSV file
-values = []
-scope.write('SAVe:WAVEform:FILEFormat SPREADSheet')
-
-for channel in channels:
-    # Select source
-    scope.write('DATa:SOUrce CH' + str(channel))
-    values = scope.query_binary_values('CURV?', datatype='b', is_big_endian=True)
-    print(values)
-
-    # Construct filename
-
-    # Plot valaues
-    plt.plot(values)
-
-# TODO: use SAVe:WAVEform instead - it creates header automatically
-scope.write('SAVe:WAVEform ALL, "E:/' + fileName + '.csv')
-
-plt.savefig(fileName + '.png')
-# plt.show()
-
-# Start infinite loop
-i=0
-while i < 1:
-    # scope.assert_trigger()
-    # scope.waitait_for_srq()    
-    scope.write('TRIGger:STATE?')
-    triggerState = scope.read()
-    print(triggerState)
 
 rm.close()
