00a086c003275672535e2a1cdf948b2edd403e8d
Petr Stepanov <stepanovps@gmail.com>
2022-03-08 18:14:44 -0500

    Updates

--- test.py
+++ test.py
@@ -4,65 +4,109 @@
 import pyvisa
 import matplotlib.pyplot as plt
 import numpy
+from datetime import datetime
 # import usb
+
+def writeFileHeader(inst):
+    print('Model,')
+
+#def readDecode(scope, string):
+#    # https://pyvisa.readthedocs.io/en/latest/introduction/communication.html
+#    # Query '?' tests both reading and writing    
+#    scope.write(string)
+#    # We need to decode string in Python3: https://stackabuse.com/convert-bytes-to-string-in-python/
+#    output = scope.read()
+#    print(type(output))
+#    # str(output, 'UTF-8')
+#    return output
+
+    
 
 # Docs here: https://pyvisa.readthedocs.io/en/latest/index.html
 pyvisa.log_to_screen()
 
-# NOT WORKING
-# Create Resource Manager object with default IVI backend (no arguments)
+# Create Resource Manager
+
+# Default IVI backend (FAILS)
 # rm = pyvisa.ResourceManager()
-# rm.list_resources()
-# print(rm.list_resources('?*'))
-# print(rm.list_resources())
 
-# WORKS
-# Create Resource Manager with Py backend
-rmpy=pyvisa.ResourceManager('@py')
+# Py backend (WORKS)
+rm=pyvisa.ResourceManager('@py')
+print(rm.list_resources())
 
-# NOT WORKING
-# USB issue: Found a device whose serial number cannot be read
-# Potential solution: https://www.google.com/search?client=firefox-b-1-e&q=pyvisa+Found+a+device+whose+serial+number+cannot+be+read
-# print(rmpy.list_resources())
+# Establish a conection with the device
+# USB (NOT WORK)
+# addr = 'USB0::1689::1110::C013681::0::INSTR'Returns the instrument identification data.
 
-# WORKS
-# Ethernet approach: https://github.com/pyvisa/pyvisa-py/issues/261
-ipaddr = 'TCPIP::192.168.100.36::INSTR'
-inst = rmpy.open_resource(ipaddr)
+# Ethernet (WORKS): https://github.com/pyvisa/pyvisa-py/issues/261
+addr = 'TCPIP::192.168.100.36::INSTR'
+
+scope = rm.open_resource(addr)
 
 # Configure the instrument
 # https://pyvisa.readthedocs.io/en/latest/introduction/communication.html#getting-the-instrument-configuration-right
 # p.2-5 Programmers manual: 
 # | This oscilloscope does not support ASCII LF only message termination. The
 # | oscilloscope always terminates outgoing messages with LF and EOI.
-inst.read_termination = '\r\n'
-inst.write_termination = '\n'
+scope.read_termination = '\n'   # '\n\r', '\r\n', '\r' not work 
+scope.write_termination = '\n'
 
-# Query the device with the following message: '\*IDN?'. 
-# Which is the standard GPIB message for “what are you?” or – in some cases – “what’s on your display at the moment?”.
-# https://pyvisa.readthedocs.io/en/latest/introduction/communication.html
-inst.query('*IDN?')
+# Obtain oscilloscope identification code (manual, p.2-350):
+scope.write('*IDN?')        # 
+scopeCode = scope.read()
+print(scopeCode)
 
-# inst.query('*IDN?')
+# Tip: scope.query('*IDN?') is same as:
+# scope.write('*IDN?')
+# print(instr.read())
 
 # TEST: read raw data - success
-# inst.write('CURV?')
-# data = inst.read_raw()
+scope.write('CURV?')
+data = scope.read_raw()
 
-# TEST: what's on the display at the moment?
-# inst.query("*IDN?")
+# TEST (FAIL): if oscilloscope is configured to transfer data in ASCII
+# values = scope.query_ascii_values('CURV?')
+# print(values)
 
+# TEST: if oscilloscope is configured to transfer data in ASCII
+# Binary data typ[es list: https://docs.python.org/3/library/struct.html#format-characters
+channels = [1, 2]
+now = datetime.now()    # remember current time
 
+plt.clf()               # configure plot
+plt.grid(True)
+plt.xlabel('Channel')
+plt.ylabel('Amplitude, mV')
+fileName="/cuanas/Data/" + now.strftime("%Y-%m-%d-%H:%M:%S")
 
-# TEST: acquire waveform from the scope and plot
-# values0 = inst.query_ascii_values('CURV?')
-# print(values0)
+# save CSV file
+values = []
+scope.write('SAVe:WAVEform:FILEFormat SPREADSheet')
 
-# values1 = inst.query_ascii_values('CURV?', container=numpy.array)
-# print(values1)
+for channel in channels:
+    # Select source
+    scope.write('DATa:SOUrce CH' + str(channel))
+    values = scope.query_binary_values('CURV?', datatype='b', is_big_endian=True)
+    print(values)
 
-# values2 = inst.query_ascii_values('CURV?', container=numpy.array, converter='x')
-# print(values2)
+    # Construct filename
 
-#plt.plot(values)
-#plt.show()
+    # Plot valaues
+    plt.plot(values)
+
+# TODO: use SAVe:WAVEform instead - it creates header automatically
+scope.write('SAVe:WAVEform ALL, "E:/' + fileName + '.csv')
+
+plt.savefig(fileName + '.png')
+# plt.show()
+
+# Start infinite loop
+i=0
+while i < 1:
+    # scope.assert_trigger()
+    # scope.waitait_for_srq()    
+    scope.write('TRIGger:STATE?')
+    triggerState = scope.read()
+    print(triggerState)
+
+rm.close()
