8f61599b2a66d13f665c3793bba5b90489033a4a
Petr Stepanov <stepanovps@gmail.com>
2022-03-08 21:17:01 -0500

    updates

--- test.py
+++ test.py
@@ -2,20 +2,23 @@
 # because nivisa requires python version > ...
 # echo 'set path = ($path /usr/local/bin $HOME/bin .)' >> ~/.login
 import pyvisa
+import time
 import matplotlib.pyplot as plt
 import numpy
 from datetime import datetime
 # import usb
 
-def writeFileHeader(inst):
-    print('Model,')
+# HELPER FUNCTIONS ----------------------------------------------------------------------------------
+
+#def writeFileHeader(inst):
+#    print('Model,')
 
 #def readDecode(scope, string):
 #    # htt
 
-def handle_event(resource, event, user_handle):
-    resource.called = True
-    print(f"Handled event {event.event_type} on {resource}")
+#def handle_event(resource, event, user_handle):
+#    resource.called = True
+#    print(f"Handled event {event.event_type} on {resource}")
     
 #    # Query '?' tests both reading and writing    
 #    instr.write(string)
@@ -25,22 +28,64 @@ def handle_event(resource, event, user_handle):
 #    # str(output, 'UTF-8')
 #    return output
 
+# Function returns integer caquisition state
+def getAcquisitionState(instr):
+    # 'ACQuire?' returns the following current acquisition parameters:
+    # Stop after, Acquisition state, Mode, Number of averages, Sampling mode
+    instr.write('ACQuire?')
+    acquisitionParamsString = instr.read()
+    # acquisitionParams = "RUNST;1;SAM;INFI;512;2.5000E+9;0;TEMPE:" Here, 1 (second parameter) is acquisition state
+    acquisitionParams = acquisitionParamsString.split(';')
+    acquisitionState = acquisitionParams[1]
+    return int(acquisitionState)
+
+# Function builds filename from current date
+def buildFileName():
+    now = datetime.now()    # remember current time
+    fileName = now.strftime("%Y-%m-%d-%H-%M-%S")
+    return fileName
+
+# Function saves CH1 and CH2 as plots to NAS
+def savePlotToNas(fileName):
+    # Configure PyPlot
+    plt.clf()
+    plt.grid(True)
+    plt.xlabel('Channel')
+    plt.ylabel('Amplitude, mV')
+
+    # Specify channels to save
+    channels = [1, 2]    
     
+    for channel in channels:
+        # Select source
+        instr.write('DATa:SOUrce CH' + str(channel))
+        # Binary data types list: https://docs.python.org/3/library/struct.html#format-characters
+        values = instr.query_binary_values('CURV?', datatype='b', is_big_endian=True)
+        #print(values)
+    
+        # Add values to plot
+        plt.plot(values)
+    
+    # Save plot as picture to NAS
+    plt.savefig('/cuanas/Data/' + fileName + '.png')
+    # plt.show()
+
+# MAIN FUNCTION ----------------------------------------------------------------------------------
+# Turn on PyVisa debug mode
+# pyvisa.log_to_screen()
 
 # Docs here: https://pyvisa.readthedocs.io/en/latest/index.html
-pyvisa.log_to_screen()
 print("PyVisa version", pyvisa.__version__)
-# Create Resource Manager
 
+# Create Resource Manager
 # Default IVI backend (FAILS)
 # rm = pyvisa.ResourceManager()
-
 # Py backend (WORKS)
 rm=pyvisa.ResourceManager('@py')
 print(rm.list_resources())
 
 # Establish a conection with the device
-# USB (NOT WORK)
+# USB (FAILS)
 # addr = 'USB0::1689::1110::C013681::0::INSTR'Returns the instrument identification data.
 
 # Ethernet (WORKS): https://github.com/pyvisa/pyvisa-py/issues/261
@@ -48,8 +93,6 @@ addr = 'TCPIP::192.168.100.36::INSTR'
 
 # Open oscilloscope
 with rm.open_resource(addr) as instr:
-
-    
     # Configure the instrument
     # https://pyvisa.readthedocs.io/en/latest/introduction/communication.html#getting-the-instrument-configuration-right
     # p.2-5 Programmers manual: 
@@ -58,69 +101,76 @@ with rm.open_resource(addr) as instr:
     instr.read_termination = '\n'   # '\n\r', '\r\n', '\r' not work 
     instr.write_termination = '\n'
     
-    # Obtain oscilloscope identification code (manual, p.2-350):
-    instr.write('*IDN?')        # 
+    # TEST: Obtain oscilloscope identification code (manual, p.2-350):
+    instr.write('*IDN?')
     scopeCode = instr.read()
     print(scopeCode)
     
-    # Tip: instr.query('*IDN?') is same as:
-    # instr.write('*IDN?')
+    # Tip: instr.query('...') is same as:
+    # instr.write('...')
     # print(instr.read())
     
     # TEST: read raw data - success
     instr.write('CURV?')
     data = instr.read_raw()
     
-    # TEST (FAIL): if oscilloscope is configured to transfer data in ASCII
+    # TEST (FAIL): if oscilloscope is configured to transfer data in ASCII (its probably not)
     # values = instr.query_ascii_values('CURV?')
     # print(values)
     
-    # TEST: if oscilloscope is configured to transfer data in ASCII
-    # Binary data typ[es list: https://docs.python.org/3/library/struct.html#format-characters
-    channels = [1, 2]
-    now = datetime.now()    # remember current time
-    
-    plt.clf()               # configure plot
-    plt.grid(True)
-    plt.xlabel('Channel')
-    plt.ylabel('Amplitude, mV')
-    fileName = now.strftime("%Y-%m-%d-%H-%M-%S")
-    
-    # save CSV file
-    values = []
-    instr.write('SAVe:WAVEform:FILEFormat SPREADSheet')
-    
-    for channel in channels:
-        # Select source
-        instr.write('DATa:SOUrce CH' + str(channel))
-        values = instr.query_binary_values('CURV?', datatype='b', is_big_endian=True)
-        print(values)
-    
-        # Construct filename
-    
-        # Plot valaues
-        plt.plot(values)
-    
-    # TODO: use SAVe:WAVEform instead - it creates header automatically
-    instr.write('SAVe:WAVEform:FILEFormat SPREADSheet')
-    instr.write('SAVe:WAVEform ALL, "E:/' + fileName + '.csv"')
-    
-    plt.savefig('/cuanas/Data/' + fileName + '.png')
-    # plt.show()
-    
-    # Start infinite loop
-    #i=0
-    scope = instr
-    while i < 3:
+    # Capture trigger events (infinite loop)
+    i = 0
+    while i < 1:
+        # Obtain acquisition state (not needed?)
+        # acqState = getAcquisitionState(instr)       
+        # print("Acquisition state is " + str(acqState))   
+
+        # Obtain trigger state (manual, p.2-930)
+        # REA (redy)     - oscilloscope is ready to accept a trigger.
+        # ARM (armed)    - indicates that the oscilloscope is acquiring pretrigger information.
+        # TRIG (trigger) - scope triggered, triggered and is acquiring the post trigger information.       
+
+        instr.write('TRIGger:STATE?')
+        triggerState = instr.read()
+        if triggerState == "TRIG":
+            fileName = buildFileName()
+            print("Trigger state is " + triggerState + ". Saving waveform '" + fileName + "'")
+
+            # Save waveforms to jumpdrive or network drive mounted to the scope
+            instr.write('SAVe:WAVEform:FILEFormat SPREADSheet')
+            instr.write('SAVe:WAVEform ALL, "E:/' + fileName + '.csv"')
+            
+            # Save plots to NAS
+            savePlotToNas(fileName)
+
+        # Sleep 250 ms
+        time.sleep(0.25)
+
+'''
+acqState = 1
+while acqState==1:
+    acqState = getAcquisitionState(instr)
+    print("Acquisition state is " + str(acqState))
+    time.sleep(0.5) # 0.5 s
+print("Acquisition state is " + str(acqState))
+'''
+
+
     #instr.write('TRIGger:STATE?')
     #triggerState = instr.read()
     #print(triggerState)
+
+
+# TRIGger:STATE?      p.960
+
+
+'''
         waiting = 1
         looptime = datetime.now()
         
         # wait here until trigger received, forced trigger occurs or time ends
         while (waiting == 1):
-            waiting = int(scope.ask("ACQ:STATE?"))
+            waiting = int(scope.write('ACQuire?'))
             #capture the trigger time
             if waiting == 0:
                 triggertime = datetime.now()
@@ -128,7 +178,8 @@ with rm.open_resource(addr) as instr:
                 if scope.query("TRIGGER:STATE?") == 'READY':
                     print("No trigger event for finale capture")
                     break
-
+        i = i+1
+'''
 
 '''
 # PyVisa wait for trigger: https://pyvisa.readthedocs.io/en/latest/introduction/example.html
